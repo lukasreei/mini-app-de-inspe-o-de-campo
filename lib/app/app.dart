253948cab2f0303afb1beca45f36b307e3c1d4b1
lukasreei <lukas581800@gmail.com>
2026-09-07 22:31:45 -0300

    criacao iniial de app

---
+++ lib/app/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inspeção de Campo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Inspeção de Campo'),
+        ),
+      ),
+    );
+  }
+}
